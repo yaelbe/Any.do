e5b0dc4a837a31d068a83ab86e2c0fcc968d5196
yael bilu <yaeleran@gmail.com>
2020-12-09 18:23:33 +0200

    add shadow to cell

--- Any.do/GroceryView/Cell/GroceryCellView.swift
+++ Any.do/GroceryView/Cell/GroceryCellView.swift
@@ -10,6 +10,10 @@ import UIColor_Hex_Swift
 
 class GroceryCellView: UICollectionViewCell {
     
+    struct consts {
+        static let cornerRadius: CGFloat = 10
+    }
+    
     @IBOutlet weak var topView: UIView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
@@ -17,12 +21,24 @@ class GroceryCellView: UICollectionViewCell {
     static let identifier: String = "GroceryCellView"
     
     static var nib: UINib {
-           return UINib(nibName: identifier, bundle: Bundle(for: self))
+        return UINib(nibName: identifier, bundle: Bundle(for: self))
     }
-    override func awakeFromNib() {
-        contentView.layer.borderWidth = 1
-        contentView.layer.borderColor = UIColor.black.cgColor
-        contentView.layer.cornerRadius = 10
+    
+    override func layoutSubviews() {
+        //Add shadow
+        layer.shadowColor = UIColor.lightGray.cgColor
+        layer.shadowOffset = CGSize(width: 2.0, height: 5.0)
+        layer.shadowRadius = 2.0
+        layer.shadowOpacity = 0.5
+        layer.masksToBounds = false
+        layer.shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: consts.cornerRadius).cgPath
+        layer.backgroundColor = UIColor.clear.cgColor
+
+        contentView.layer.masksToBounds = true
+        contentView.layer.cornerRadius = consts.cornerRadius
+        layer.cornerRadius = consts.cornerRadius
+        
+        backgroundColor = UIColor.white
     }
     
     func setup( grocery: Grocery) {
